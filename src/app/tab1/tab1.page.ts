62d0072f2ad45e486b97aafed4ec3d788b10553f
wylliam <wyliam.novais@gmail.com>
2020-04-17 01:03:33 -0300

    :bulb: Grafico de Infectados e Falecidos

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -20,15 +20,15 @@ export class Tab1Page {
 
 
   ionViewDidEnter() {
-    this.createBarChart();
     this.consultarDadosAtuais();
   }
+
   constructor(private httpClient: HttpClient) { }
 
   private REST_API_COVID_ATUAL = "https://api.apify.com/v2/key-value-stores/TyToNta7jGKkpszMZ/records/LATEST?disableRedirect=true";
 
-  public consultarDadosAtuais() {
-    this.httpClient.get(this.REST_API_COVID_ATUAL).subscribe(
+   public async consultarDadosAtuais() {
+    await this.httpClient.get(this.REST_API_COVID_ATUAL).subscribe(
       data => {
 
         this.dadosCovid.dataAtualizacao = data.lastUpdatedAtSource;
@@ -54,38 +54,27 @@ export class Tab1Page {
         })
 
         console.log(this.dadosCovid);
-
-
-
+        this.createBarChart();
 
       }
     );
   }
 
   createBarChart() {
-    // this.bars = new Chart(this.barChart.nativeElement, {
-    //   type: 'bar',
-    //   data: {
-    //     labels: ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8'],
-    //     datasets: [{
-    //       label: 'Viewers in millions',
-    //       data: [2.5, 3.8, 5, 6.9, 6.9, 7.5, 10, 17],
-    //       backgroundColor: 'rgb(38, 194, 129)', // array should have same number of elements as number of dataset
-    //       borderColor: 'rgb(38, 194, 129)',// array should have same number of elements as number of dataset
-    //       borderWidth: 1
-    //     }]
-    //   },
-    //   options: {
-    //     scales: {
-    //       yAxes: [{
-    //         ticks: {
-    //           beginAtZero: true
-    //         }
-    //       }]
-    //     }
-    //   }
-    // });
-  }
+    this.bars = new Chart(this.barChart.nativeElement, {
+      type: 'pie',
+      data: {
+        labels: ['Infectados', 'Falecidos'],
+        datasets: [{
+          label: 'Infectados x Falecidos',
+          data: [this.dadosCovid.infectado, this.dadosCovid.falecido],
+          backgroundColor: ['red', 'green'],
+          borderColor: 'black',
+          borderWidth: 1
+        }]
+      }
+    });
+}
 
 
 }
